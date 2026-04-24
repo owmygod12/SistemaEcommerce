650fa7729ba065e807bd3b1e6182b218cf176980
Henrique Lopes Borges <135183107+owmygod12@users.noreply.github.com>
2026-04-24 00:48:23 -0300

    Update Boleto.java

--- Projeto Ecomerce/src/Boleto.java
+++ Projeto Ecomerce/src/Boleto.java
@@ -1,2 +1,14 @@
-public class Boleto {
+public class Boleto extends FormaPagamento {
+    private String codigoBarras;
+
+    public Boleto(String descricao, String codigoBarras) {
+        super(descricao);
+        this.codigoBarras = codigoBarras;
+    }
+
+    @Override
+    public boolean pagar(double valor) {
+        System.out.println("Gerando boleto com código: " + this.codigoBarras);
+        return true;
+    }
 }
